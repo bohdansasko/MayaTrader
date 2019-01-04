e7fc8e56aca9e6ed3fc1c608e597307ce5c0985f
TQ0oS <0000xffff@gmail.com>
2019-01-04 04:46:51 +0200

    Checklist 2018 pre-NewYear - add ads in Alerts list

--- exmo-ios-client/sources/Alerts/Main/View/AlertsViewController.swift
+++ exmo-ios-client/sources/Alerts/Main/View/AlertsViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class AlertsViewController: ExmoUIViewController, AlertsViewInput {
     var output: AlertsViewOutput!
     var listView: AlertsListView!
+    var bannerView: GADBannerView!
     
     var btnCreateAlert: UIBarButtonItem = {
         return UIBarButtonItem(image: UIImage(named: "icNavbarPlus"),
@@ -24,6 +26,7 @@ class AlertsViewController: ExmoUIViewController, AlertsViewInput {
         super.viewDidLoad()
         titleNavBar = "Alerts"
         setupViews()
+        bannerView.load(GADRequest())
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -46,6 +49,8 @@ extension AlertsViewController {
         
         setupNavigationBar()
         setupConstraints()
+        
+        setupBannerView()
     }
     
     private func setupNavigationBar() {
@@ -58,5 +63,42 @@ extension AlertsViewController {
     
     private func setupConstraints() {
         listView.anchor(view.layoutMarginsGuide.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 10, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    }
+    
+    func setupBannerView() {
+        bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        bannerView.delegate = self
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+    }
+    
+    func addBannerToView(_ bannerView: GADBannerView) {
+        view.addSubview(bannerView)
+        bannerView.anchor(nil, left: view.layoutMarginsGuide.leftAnchor, bottom: view.layoutMarginsGuide.bottomAnchor, right: view.layoutMarginsGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    }
+
+}
+
+// @MARK: GADBannerViewDelegate
+extension AlertsViewController: GADBannerViewDelegate {
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("adViewDidReceiveAd")
+        if let _ = bannerView.superview {
+            bannerView.alpha = 0
+            UIView.animate(withDuration: 1) {
+                bannerView.alpha = 1
+                self.listView.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: -54).isActive = true
+            }
+        } else {
+            addBannerToView(bannerView)
+        }
+    }
+    
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print(error)
+        UIView.animate(withDuration: 1) {
+            bannerView.alpha = 0
+            self.listView.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: 0).isActive = true
+        }
     }
 }
