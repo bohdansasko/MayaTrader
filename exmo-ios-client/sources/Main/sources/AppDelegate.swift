fc7a4ed78e58228890722f3fb409c15898b6de06
TQ0oS <0000xffff@gmail.com>
2019-02-08 23:35:04 +0200

    Flash EX-40. List of alerts is empty after interrupted connection.

--- exmo-ios-client/sources/Main/sources/AppDelegate.swift
+++ exmo-ios-client/sources/Main/sources/AppDelegate.swift
@@ -52,7 +52,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        AppDelegate.vinsoAPI.disconnect()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
