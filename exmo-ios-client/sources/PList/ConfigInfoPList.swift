5a8601fc40c1807a34a13c93f0f588a316370c25
TQ0oS <0000xffff@gmail.com>
2019-02-11 00:53:29 +0200

    TODO BEFORE RELEASE - rename & group files -

---
+++ exmo-ios-client/sources/PList/ConfigInfoPList.swift
@@ -0,0 +1,53 @@
+//
+//  InfoPList.swift
+//  exmo-ios-client
+//
+
+import Foundation
+
+struct ConfigInfoPList: Codable {
+    let appVersion: String
+    let buildVersion: String
+    let configuration: Configuration
+    
+    enum CodableKeys: String, CodingKey {
+        case appVersion = "CFBundleShortVersionString"
+        case buildVersion = "CFBundleVersion"
+        case configuration
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodableKeys.self)
+        appVersion = try container.decode(String.self, forKey: CodableKeys.appVersion)
+        buildVersion = try container.decode(String.self, forKey: CodableKeys.buildVersion)
+        configuration = try container.decode(Configuration.self, forKey: CodableKeys.configuration)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodableKeys.self)
+        try container.encode(appVersion, forKey: CodableKeys.buildVersion)
+        try container.encode(buildVersion, forKey: CodableKeys.buildVersion)
+        try container.encode(configuration, forKey: CodableKeys.configuration)
+    }
+    
+}
+
+extension ConfigInfoPList {
+    struct Configuration: Codable {
+        let endpoint: String
+        
+        enum ConfigCodableKeys: String, CodingKey {
+            case endpoint
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: ConfigCodableKeys.self)
+            endpoint = try container.decode(String.self, forKey: ConfigCodableKeys.endpoint)
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: ConfigCodableKeys.self)
+            try container.encode(endpoint, forKey: ConfigCodableKeys.endpoint)
+        }
+    }
+}
