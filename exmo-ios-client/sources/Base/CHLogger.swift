e96b89859c9442ce9a0f7ccf2abca08bc412253a
bohdans <0000xffff@gmail.com>
2019-09-07 15:33:49 +0300

    add date & time for logger

--- exmo-ios-client/sources/Base/CHLogger.swift
+++ exmo-ios-client/sources/Base/CHLogger.swift
@@ -17,7 +17,7 @@ struct log {
             let fileName = (pathToFile as NSString).lastPathComponent
             let argsAsStr = items.map { String(describing: $0) }.joined(separator: " ")
             let threadAsStr = Thread.current.isMainThread ? "thread: <main>" : "thread: <background>"
-            print(level.icon, threadAsStr, line, fileName, argsAsStr)
+            print(level.icon, Date().now, threadAsStr, line, fileName, argsAsStr)
         #endif
     }
 
